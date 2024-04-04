175d68c92a928c4303a84274e7d56f3fd1107dae
Eknathreddy09 <eknath.reddy09@gmail.com>
2024-04-04 16:13:50 +0530

    Update PartnertapdemoApplication.java

--- src/main/java/com/partnertapdemo/partnertapdemo/PartnertapdemoApplication.java
+++ src/main/java/com/partnertapdemo/partnertapdemo/PartnertapdemoApplication.java
@@ -1,8 +1,6 @@
 package com.partnertapdemo.partnertapdemo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -11,9 +9,5 @@ public class PartnertapdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PartnertapdemoApplication.class, args);
-	}
-	@Bean
-	public HttpTraceRepository htttpTraceRepository() {
-		return new InMemoryHttpTraceRepository();
 	}
 }
